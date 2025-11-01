2b898149bd291b21a3bef9be06b1408f8117a8ae
Sarsenov <zhanbek.sarsenov@gmail.com>
2025-11-02 01:56:16 +0500

    Реалізує завдання

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import model.MyArea;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введіть радіус кола: ");
+        double radius = scanner.nextDouble();
+        scanner.close();
 
+        double area = MyArea.areaOfCircle(radius);
+        System.out.println("Площа кола радіуса " + radius + " дорівнює " + Double.toString(area));
     }
 }
---
+++ src/model/MyArea.java
@@ -0,0 +1,9 @@
+package model;
+
+public class MyArea {
+    public static final double PI = Math.PI;
+
+    public static double areaOfCircle(double radius) {
+        return PI * radius * radius;
+    }
+}
